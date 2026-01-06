666837eed9c3e05d093d21183f80243b4dd6c694
harshitMrt <hritikgulia.mrt@gmail.com>
2026-01-06 15:39:57 +0530

    LinkedIn Post Generator

---
+++ llm_helper.py
@@ -0,0 +1,12 @@
+import dotenv
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+if __name__ == "__main__":
+    result = llm.invoke("Main ingredients in samosa")
+    print(result.content)
---
+++ preprocess.py
@@ -0,0 +1,86 @@
+import json
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from llm_helper import llm
+
+def process_data(raw_file_path, processed_file_path="Data/raw_posts.json"):
+    enriched_posts=[]
+    with open (raw_file_path) as file:
+        posts = json.load(file)
+        for post in posts:
+            metadata= extract_metadata(post['text'])
+            post_with_metadata= post | metadata
+            enriched_posts.append(post_with_metadata)
+
+    unified_tags = get_unified_tags(enriched_posts)
+
+    for post in enriched_posts:
+        current_tags = post['tags']
+        new_tags = {unified_tags[tag] for tag in current_tags}
+        post['tags'] = list(new_tags)
+
+    with open(processed_file_path, encoding='utf-8', mode="w") as outfile:
+        json.dump(enriched_posts, outfile, indent=4)
+
+def get_unified_tags(post_with_metadata):
+    unique_tags=set()
+    for post in post_with_metadata:
+        unique_tags.update(post['tags'])
+
+    unique_tags_list = ", ".join(unique_tags)
+
+    template = '''I will give you a list of tags. You need to unify tags with the following requirements,
+        1. Tags are unified and merged to create a shorter list. 
+           Example 1: "Jobseekers", "Job Hunting" can be all merged into a single tag "Job Search". 
+           Example 2: "Motivation", "Inspiration", "Drive" can be mapped to "Motivation"
+           Example 3: "Personal Growth", "Personal Development", "Self Improvement" can be mapped to "Self Improvement"
+           Example 4: "Scam Alert", "Job Scam" etc. can be mapped to "Scams"
+        2. Each tag should be follow title case convention. example: "Motivation", "Job Search"
+        3. Output should be a JSON object, No preamble
+        3. Output should have mapping of original tag and the unified tag. 
+           For example: {{"Jobseekers": "Job Search",  "Job Hunting": "Job Search", "Motivation": "Motivation}}
+
+        Here is the list of tags: 
+        {tags}
+        '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input = {'tags': unique_tags_list})
+
+    try:
+        json_parser=JsonOutputParser()
+        result = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Invalid post")
+
+    return result
+
+def extract_metadata(text):
+    template = '''
+    You are given a linkedIn post. You need to extract number of lines, language of post, and tags.
+    1. return a valid JSON. No Preamble
+    2. JSON object should have excatly have 3 keys : line_count, language, tags
+    3. tags is an array of text tags. Extract maximum of 4 tags and minimun of 2 tags.
+    4. Language should be English or Hinglish ( Hinglish means Hindi + English )
+    
+    Here is an actual post on which you need to perform the above tasks 
+    {post} 
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+
+    response = chain.invoke(input={'post': text})
+
+    try:
+        json_parser = JsonOutputParser()
+        rest = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Invalid post")
+
+    return rest
+
+if __name__ == "__main__":
+    process_data("Data/raw_posts.json", "Data/processed_posts.json")
